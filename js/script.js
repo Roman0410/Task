89c028b8eb34218b4bfd4a0c2f460d0e74d8dd23
Roman0410 <100242100+Roman0410@users.noreply.github.com>
2022-11-29 21:20:32 +0200

    Task

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 const confiq = {
   0: {
-    src: "../img/Marketing.jpg",
+    src: "./img/Marketing.jpg",
     Name: "Кевін",
     Number: "+38 (097) 001 01 00",
   },
   1: {
-    src: "../img/Front_end.jpg",
+    src: "./img/Front_end.jpg",
     Name: "Клара",
     Number: "+38 (097) 221 21 22",
   },
   2: {
-    src: "../img/ui.jpg",
+    src: "./img/ui.jpg",
     Name: "Джон",
     Number: "+38 (097) 331 31 33",
   },
